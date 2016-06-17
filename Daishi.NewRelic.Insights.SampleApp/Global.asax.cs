5170c01cd931bb90d4e1dab93b20e4fda5280270
Paul Mooney <paul@daishisystems.com>
2016-06-17 09:46:43 +0100

    Updated Nuget package

--- Daishi.NewRelic.Insights.SampleApp/Global.asax.cs
+++ Daishi.NewRelic.Insights.SampleApp/Global.asax.cs
@@ -17,9 +17,9 @@ namespace Daishi.NewRelic.Insights.SampleApp
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
-            NewRelicInsightsClient.Instance.NewRelicInsightsMetadata.AccountID = "646832";
+            NewRelicInsightsClient.Instance.NewRelicInsightsMetadata.AccountID = "{New Relic Account ID}";
             NewRelicInsightsClient.Instance.NewRelicInsightsMetadata.APIKey =
-                "bx97bcRZ0nVQSb80O5GcVtyYREbQLNSz";
+                "{New Relic API key}";
             NewRelicInsightsClient.Instance.NewRelicInsightsMetadata.URI =
                 new Uri("https://insights-collector.newrelic.com/v1/accounts");
 
